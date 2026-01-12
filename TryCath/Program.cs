f363228d162b121f31500bb232656316c9d0c43a
Rafael Martirosyan <mar.rafael89@gmail.com>
2026-01-12 21:39:06 +0400

    Добавьте файлы проекта.

---
+++ TryCath/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace std;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter a number.");
+        var input = Console.ReadLine();
+
+        try
+        {
+            int converted = Convert.ToInt32(input);
+            Console.WriteLine(converted * 10);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("The input is not a number.");
+        }
+        catch(OverflowException)
+        {
+            Console.WriteLine("Max or min.");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("The program is finished.");
+        }
+    }
+}
